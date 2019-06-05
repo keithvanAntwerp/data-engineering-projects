ddd723fecf46493cbc9c19fdc7c51af615723f7e
keithzu <keith@zucchabar.com>
2019-06-05 00:40:21 -0700

    tested against datetime.fromtimestamp()

--- NEOSchema/NEOSchema.py
+++ NEOSchema/NEOSchema.py
@@ -1,14 +1,15 @@
 from datetime import datetime
-import math
 
 def jS_ts_splitter(ts: int):
 
     '''Return a python datetime object with the millisecond resolution of
     an input int representing the number of milliseconds since
-    Midnight, January 1, 1970 local time.
+    January 1, 1970 UTC.
+
+    Matches datatime.fromtimestamp(ints/1000)
 
     This python datetime objects maximum sample frequency is 
-    1 million Hz, or 1 megahurtz.
+    1 million Hz, or 1 megaHz.
    
     This is an inefficient brute force converter of 
     a local-time javaScript Date in int form with the
@@ -24,82 +25,86 @@ def jS_ts_splitter(ts: int):
     At any rate, it is a neat and informative way to explore the nuances of
     time and algorithmic complexity.
     '''
-
-    
     #total_micro_sec = ts*(10**3)
     
+    #correct for January 1, 1970 UTC time
+    us_corr = 8*60*60*1000000
+    
     final_count_down = ts*(10**3)
-    
-    #step 1: count the ms in years, oh how the time passes...
-    ms_in_365_year = 365*24*60*60*1000000
-    ms_in_Lep_year = 366*24*60*60*1000000
-    
-    years1970 = 0
-    #account for leap year
-    while final_count_down > ms_in_365_year:
-        year = 1970
-        if bool(year%4):
-            final_count_down -= ms_in_365_year
-        else: 
-            final_count_down -= ms_in_Lep_year
+    if (final_count_down - us_corr) > 0:
+        final_count_down = final_count_down - us_corr
+    else: final_count_down = 0
         
-        years1970 +=1
+    print(final_count_down)
+    #step 1: count the ms in years, oh how the time passes...
+    us_in_365_year = 365*24*60*60*1000000
+    us_in_Lep_year = 366*24*60*60*1000000
+    
+    year = 1970
+    
+    #account for leap year
+    while final_count_down > us_in_365_year:
+        
+        if bool(year%4):
+            final_count_down -= us_in_365_year
+        elif not bool(year%4): 
+            final_count_down -= us_in_Lep_year
+        
+        year +=1
     
     #the number of years in yearss1970loops
-    fyears = int(years1970 + year)
+    fyears = int(year)
     
     #step 2a: count the months
-    ms_in_day = ms_in_365_year/365
+    us_in_day = int(us_in_365_year/365)
+    print(us_in_day)
+   
     ######this could be made more efficent
     if bool(fyears%4):
+        monthsy = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    else: monthsy = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         
-        monthsy = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        ms_in_month = [x*ms_in_day for x in monthsy]
+    us_in_month = [x*us_in_day for x in monthsy]
+    month_ind = 0
         
-        month_ind = 0
+    while final_count_down > us_in_month[month_ind]:
+        final_count_down -= us_in_month[month_ind]
         
-        while final_count_down > ms_in_month[month_ind]:
-            final_count_down -= ms_in_month[month_ind]
-            month_ind += 1
-            
-    else:
-        monthsyl = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        ms_in_month = [x*ms_in_day for x in monthsyl]
-        #month_ind = 0
-        while final_count_down < ms_in_month[month_ind]:
-            final_count_down -= ms_in_month[month_ind]
-            month_ind += 1
+        month_ind += 1
     ######  
     fmonth = month_ind + 1
     
     #step 2b: count the days
-    ms_in_day = ms_in_365_year/365
-    fdays = (math.floor(final_count_down/ms_in_day))
-    final_count_down -= fdays*ms_in_day
+    fdays = int(final_count_down//us_in_day)
+    final_count_down -= fdays*us_in_day
+    fdays += 1
     
     #step 3: count the hours
-    ms_in_hour = ms_in_day/24
-    fhour = math.floor(final_count_down/ms_in_hour)
-    final_count_down -= fhour*ms_in_hour
+    us_in_hour = int(us_in_day/24)
+    fhour = int(final_count_down//us_in_hour)
+    final_count_down -= fhour*us_in_hour
     
     #step 4: count the minutes
-    ms_in_minute = ms_in_hour/60
-    fmins = math.floor(final_count_down/ms_in_minute)
-    final_count_down -= fmins*ms_in_minute
+    us_in_minute = int(us_in_hour/60)
+    fmins = int(final_count_down//us_in_minute)
+    final_count_down -= fmins*us_in_minute
     
     #step 5: seconds
-    ms_in_secs = ms_in_minute/60
-    fsecs = math.floor(final_count_down/ms_in_secs)
-    final_count_down -= fsecs*ms_in_secs
+    us_in_secs = us_in_minute/60
+    fsecs = int(final_count_down//us_in_secs)
+    final_count_down -= fsecs*us_in_secs
     
     #step 6: count the microsecounds
-    ms_in_ms = ms_in_secs*(10**(-6))
-    fms = math.floor(final_count_down/ms_in_ms)
-    final_count_down -= fms*ms_in_secs
+    us_in_us = int(us_in_secs*(10**(-6)))
+    fus = int(final_count_down//us_in_us)
+    final_count_down -= fus*us_in_secs
     
     #toggle printing by uncommenting line below for now
-    #print((fyears, fmonth, fdays, fhour, fmins, fsecs, fms))
+    #print((type(fyears), type(fmonth), type(fdays), type(fhour), type(fmins), type(fsecs), type(fus)),
+          #(fyears, fmonth, fdays, fhour, fmins, fsecs, fus))
     
-    return datetime(fyears, fmonth, fdays, fhour, fmins, fsecs, fms)
+    return datetime(fyears, fmonth, fdays, fhour, fmins, fsecs, fus)
+        
+   
         
    
